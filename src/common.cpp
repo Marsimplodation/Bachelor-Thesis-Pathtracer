b6dfa175d6860c8620f6e7027afaf2cb80d27261
Marius Werkmeister <m.werkmeister@tu-braunschweig.de>
2024-03-26 20:43:25 +0100

    switched fast random for xorShift to pcg

--- src/common.cpp
+++ src/common.cpp
@@ -1,12 +1,14 @@
 #include "common.h"
+#include <cstdint>
 
 u32 hashCoords(int x, int y) {
     return (static_cast<u32>(x) << 16 | y);
 }
 
 float fastRandom(u32 & seed) {
-    seed ^= seed << 13; //xor with const;
-    seed ^= seed >> 17; //xor with const;
-    seed ^= seed << 5; //xor with const;
-    return static_cast<float>(seed % 1000)/1000.0f;
+    //using PCG HASH
+    u32 state = seed * 747796405u + 2891336453u;
+    u32 word = ((state >> ((state >> 28u) + 4u))^state) * 277803737u;
+    seed = (word >> 22u) ^ word;
+    return (float)seed / (float)UINT32_MAX;
 }
